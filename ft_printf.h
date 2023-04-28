152142266396c7de03029276dbe414d4cc5ee58d
Diogo Jorge Duarte Gomes <dduarte-@c1r10s4.42porto.com>
2023-04-28 11:03:07 +0100

    add some more functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dduarte- <dduarte-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 13:19:41 by dduarte-          #+#    #+#             */
-/*   Updated: 2023/04/28 08:58:43 by dduarte-         ###   ########.fr       */
+/*   Updated: 2023/04/28 11:01:12 by dduarte-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@
 
 int		ft_printnbr(int n);
 int		ft_printstr(char *str);
+int		ft_hex_len(unsigned int n);
+int		ft_print_hex(unsigned int n, const char format);
+int		ft_ptr_len(unsigned int n);
+int		ft_print_ptr(unsigned long long ptr);
 char	ft_printchar(char c);
 void	ft_putstr(char *str);
+void	ft_put_hex(unsigned int n, const char format);
+void	ft_put_ptr(uintptr_t n);
 
 #endif
