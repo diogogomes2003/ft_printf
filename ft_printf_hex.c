27358b23446ccfcd90dd376cb360c0a7e5dc519d
Diogo Jorge Duarte Gomes <dduarte-@c1r10s4.42porto.com>
2023-04-28 11:03:49 +0100

    add functions to print hex

---
+++ ft_printf_hex.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_hex.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduarte- <dduarte-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 09:48:24 by dduarte-          #+#    #+#             */
+/*   Updated: 2023/04/28 10:34:29 by dduarte-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+
+int	ft_hex_len(unsigned int n)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (n != 0)
+	{
+			len++;
+			n = n / 16;
+	}
+	return (len);
+}
+
+void	ft_put_hex(unsigned int n, const char format)
+{
+	if (n >= 16)
+	{
+		ft_put_hex(n / 16, format);
+		ft_put_hex(n % 16, format);
+	}
+	else
+	{
+		if (n <= 9)
+			ft_putchar_fd((n + 48), 1);
+		else
+		{
+			if (format == 'x')
+				ft_putchar_fd((n - 10 + 'a'), 1);
+			if (format == 'X')
+				ft_putchar_fd((n - 10 + 'A'), 1);
+		}
+	}
+}
+
+int	ft_print_hex(unsigned int n, const char format)
+{
+	if (n == 0)
+		return (write(1, "0", 1));
+	else
+		ft_put_hex(n, format);
+	return (ft_hex_len(n));
+}
