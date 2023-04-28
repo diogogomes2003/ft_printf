ac23bf0f507cf2e8080de34087fb7db4dca5a816
Diogo Jorge Duarte Gomes <dduarte-@c1r10s4.42porto.com>
2023-04-28 09:17:40 +0100

    add util(printchar, printstr, printnbr)

---
+++ ft_printfutils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printfutils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduarte- <dduarte-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 08:45:48 by dduarte-          #+#    #+#             */
+/*   Updated: 2023/04/28 09:12:42 by dduarte-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+#include <stdlib.h>
+
+char	ft_printchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == NULL)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printnbr(int n)
+{
+	int		len;
+	char	*num;
+
+	len = 0;
+	num = ft_itoa(n);
+	len = ft_printstr(num);
+	free(num);
+	return (len);
+}
